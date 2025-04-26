da33a58fd19bae8b4202b9033f18d42c4f3c39a1
swarup-bhol <54322967+swarup-bhol@users.noreply.github.com>
2025-04-26 18:34:43 +0530

    small changes

--- SpannerIngetion/src/test/java/com/example/SpannerIngetion/SpannerIngetionApplicationTests.java
+++ SpannerIngetion/src/test/java/com/example/SpannerIngetion/SpannerIngetionApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.SpannerIngetion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpannerIngetionApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
